ff3fe90396f06b93a262278dbd2c8a7777d9cc25
Gleomit <draychev95@mail.ru>
2015-04-02 21:00:34 +0300

    fixes and update

--- Homework - Cookies and Web Storages/Problem 2. Poll/js/pollSystem.js
+++ Homework - Cookies and Web Storages/Problem 2. Poll/js/pollSystem.js
@@ -1,10 +1,113 @@
 $(document).ready(function(){
-    var questions = [];
-    questions.push(new Question('\'.MOV\' extension refers usually to what kind of file?',
-        ['Image file', 'Animation/movie file', 'Audio file', 'MS Office document'], 2));
-    questions.push(new Question('\'OS\' computer abbreviation usually means ?',
-        ['Order of Significance', 'Open Software', 'Operating System', 'Optical Sensor'], 'Operating System'));
-    questions.push(new Question('\'.MOV\' extension refers usually to what kind of file?',
-        ['Image file', 'Animation/movie file', 'Audio file', 'MS Office document'], 2));
-    //console.log(JSON.parse(localStorage.getItem('question')));
+    if(window.localStorage != null){
+        var questions,
+            currentQuestion,
+            currentTimerLeft,
+            interval,
+            i,
+            currentAnswer;
+
+        localStorage.isCompleted = localStorage.isCompleted || false;
+        localStorage.firstTime = localStorage.firstTime || true;
+        localStorage.atQuestion = localStorage.atQuestion || 0;
+        localStorage.questions = localStorage.questions || JSON.stringify([]);
+        localStorage.timerLeft = localStorage.timerLeft || 30000;
+
+        questions = JSON.parse(localStorage.questions);
+        currentQuestion = parseInt(localStorage.atQuestion);
+        currentTimerLeft = parseInt(localStorage.timerLeft);
+        interval = setInterval(function(){
+            currentTimerLeft -= 1;
+
+            if(currentTimerLeft <= 0) {
+                localStorage.isCompleted = true;
+                clearInterval(interval);
+            }
+        }, 1);
+
+        if(JSON.parse(localStorage.firstTime) == true){
+            localStorage.questions = JSON.stringify(loadSampleQuestions());
+            questions = JSON.parse(localStorage.questions);
+            localStorage.firstTime = false;
+        }
+
+        $(window).on('unload', function(){
+            localStorage.atQuestion = currentQuestion;
+            localStorage.questions = JSON.stringify(questions);
+            localStorage.timerLeft = currentTimerLeft;
+
+            clearInterval(interval);
+        });
+
+        $('#previous').on('click', function(){
+            currentQuestion -= 1;
+
+            if(currentQuestion < 0){
+                currentQuestion = 0;
+            } else{
+                onQuestionChange();
+            }
+        });
+
+        $('#next').on('click', function(){
+            currentQuestion += 1;
+
+            if(currentQuestion > questions.length - 1){
+                currentQuestion = questions.length - 1;
+            } else{
+                onQuestionChange();
+            }
+        });
+
+        $('#theQuestion').text(questions[currentQuestion].question);
+
+        for(i = 1; i <= 4; i += 1){
+            currentAnswer = $('#answer' + i.toString());
+            currentAnswer.text(questions[currentQuestion].possibleAnswers[i - 1]);
+        }
+
+        $('main').append('<button id="clearLocalStorage" form="questionForm">Clear Local Storage Variables</button>');
+        $('#clearLocalStorage').on('click', clearLocalStorageVariables);
+
+        function loadSampleQuestions(){
+            var tempQuestions = [];
+
+            tempQuestions.push(new Question('\'.MOV\' extension refers usually to what kind of file?',
+                ['Image file', 'Animation/movie file', 'Audio file', 'MS Office document'], 1));
+            tempQuestions.push(new Question('\'OS\' computer abbreviation usually means ?',
+                ['Order of Significance', 'Open Software', 'Operating System', 'Optical Sensor'], 2));
+            tempQuestions.push(new Question('\'.JPG\' extension refers usually to what kind of file?',
+                ['System file', 'Animation/movie file', 'MS Encarta document', 'Image file'], 3));
+            tempQuestions.push(new Question('What is the term to ask the computer to put information in order numerically or alphabetically?',
+                ['Crop', 'Report', 'Record', 'Sort'], 3));
+
+            return tempQuestions;
+        }
+
+        function clearLocalStorageVariables(){
+            localStorage.removeItem('atQuestion');
+            localStorage.removeItem('firstTime');
+            localStorage.removeItem('isCompleted');
+            localStorage.removeItem('questions');
+            localStorage.removeItem('timerLeft');
+        }
+
+        function onQuestionChange(){
+            if(currentQuestion <= questions.length - 1 && currentQuestion >= 0){
+                $('#question').fadeToggle();
+
+                setTimeout(function(){
+                    $('#theQuestion').text(questions[currentQuestion].question);
+                    for(i = 1; i <= 4; i += 1){
+                        currentAnswer = $('#answer' + i.toString());
+                        currentAnswer.text(questions[currentQuestion].possibleAnswers[i - 1]);
+                    }
+                }, 300);
+
+                $('#question').fadeToggle();
+            }
+        }
+    } else {
+        $('main').append('<h1>You\'ve disable localStorage in your browser.</h1>');
+    }
 });
--- Homework - Cookies and Web Storages/Problem 2. Poll/js/scripts/Question.js
+++ Homework - Cookies and Web Storages/Problem 2. Poll/js/scripts/Question.js
@@ -1,11 +1,12 @@
-(function(){
+var Question = (function(){
     function Question(question, answersArray, theRightAnswer){
         this.question = question;
         this.isAnswered = false;
         this.choosenAnswer = "";
         this.possibleAnswers = answersArray;
         this.rightAnswer = this.possibleAnswers[theRightAnswer];
+
     }
 
     return Question;
-});
+})();
