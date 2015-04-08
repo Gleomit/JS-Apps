320e7c50a8daee87794ca2826eb9b34e1fe57e65
Gleomit <draychev95@mail.ru>
2015-04-08 21:48:44 +0300

    ---

---
+++ Homework - BaaS and Parse.com/Problem 2. List Countries/js/pageJS.js
@@ -0,0 +1,8 @@
+(function(){
+    Parse.initialize('MCYs6zTfyozcYVTr9EB3kHFT3TtftYSwhiwrjoqf', 'kQVxLosZatryKT5kYX05kAe1haoLS17iaSGrPzfl');
+    
+    var Country = Parse.Object.extend('Country');
+    var Town = Parse.Object.extend('Town');
+    var country = new Country();
+})
+
