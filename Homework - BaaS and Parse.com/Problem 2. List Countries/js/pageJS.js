8747f0ec69bbe17aa9d800cfe3b4fa3bc18fd36a
Deivid Raychev <draychev95@mail.ru>
2015-04-09 13:07:25 +0300

    ---

--- Homework - BaaS and Parse.com/Problem 2. List Countries/js/pageJS.js
+++ Homework - BaaS and Parse.com/Problem 2. List Countries/js/pageJS.js
@@ -3,6 +3,19 @@
     
     var Country = Parse.Object.extend('Country');
     var Town = Parse.Object.extend('Town');
+
     var country = new Country();
-})
+
+    var query = new Parse.Query(Country);
+    query.find({
+        success: function(result){
+            var queryTown = new Parse.Query(Town);
+
+            for(i = 0; i < result.length; i += 1){
+                console.log(result[i].get('name') + "   " + queryTown.equalTo('country', result[i]).get('name'));
+            }
+        }
+    });
+    console.log();
+})();
 
